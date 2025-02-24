4a95e2b26bcb5d57d155102431c2f69c20f718ec
Akhilesh Sharma <134252825+akhileshsharma1@users.noreply.github.com>
2025-02-24 22:27:55 +0545

    analytics

--- app/(dashboard)/_components/sidebar-item.tsx
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -2,8 +2,7 @@
 
 import { cn } from "@/lib/utils";
 import { LucideIcon } from "lucide-react";
-import { usePathname } from "next/navigation";
-import { useRouter } from "next/router";
+import { usePathname, useRouter } from "next/navigation";
 
 interface SidebarItemProps {
     icon: LucideIcon;
@@ -15,34 +14,33 @@ export const SidebarItem = ({
     icon: Icon,
     label,
     href,
-
 }: SidebarItemProps) => {
     const pathname = usePathname();
     const router = useRouter();
 
     const isActive =
-    (pathname === '/' && href === "/") || 
-    pathname === href ||
-    pathname?.startsWith(`${href}/`);
+        (pathname === '/' && href === "/") ||
+        pathname === href ||
+        pathname?.startsWith(`${href}/`);
 
     const onClick = () => {
-        router.push(href);
-    }
+        router.push(href); // This will now work properly
+    };
 
-    return(
+    return (
         <button
             onClick={onClick}
             type="button"
             className={cn(
-                "flex items-center gap-x-2 text-slate-500 test-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
+                "flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
                 isActive && "text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700"
             )}
         >
-            <div className="flex items-center center gap-x-2 py-4">
-                <Icon 
+            <div className="flex items-center gap-x-2 py-4">
+                <Icon
                     size={22}
                     className={cn(
-                        "tect-slate-500",
+                        "text-slate-500",
                         isActive && "text-sky-700"
                     )}
                 />
@@ -54,7 +52,6 @@ export const SidebarItem = ({
                     isActive && "opacity-100"
                 )}
             />
-            
         </button>
-    )
-}
+    );
+};
